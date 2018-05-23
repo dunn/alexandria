913b525fb8eb34864b07be89dbd0cd4ec09f7b5d
Alex Dunn <dunn.alex@gmail.com>
2018-05-23 14:39:11 -0700

    OAI: don't override oai_dc prefix

--- lib/oai_calisphere.rb
+++ lib/oai_calisphere.rb
@@ -2,46 +2,56 @@
 
 class OAICalisphere < OAI::Provider::Metadata::Format
   def initialize
-    @prefix = "oai_dc"
-    @schema = "https://digitalcollections.library.ucsc.edu/oai_dpla/oai_dpla.xsd"
-    @namespace = "http://www.openarchives.org/OAI/2.0/oai_dc/"
-    @element_namespace = "dc"
+    @prefix = "oai_cdl"
+    @schema = "https://alexandria.ucsb.edu/oai_cdl.xsd"
+    @namespace = "http://www.openarchives.org/OAI/2.0/"
+    @element_namespace = "cdl"
   end
 
   PREFIXES = {
     dc: {
       contributor: "all_contributors_label_sim",
-      coverage:    "location_label_tesim",
       creator:     "creator_label_tesim",
       date:        "date_si",
       description: %w[description_tesim note_label_tesim citation],
-      format:      "form_of_work_label_tesim",
-      identifier:  "uri_ssm",
+      format:      "extent_ssm",
+      identifier:  "identifier_ssm",
       language:    "language_label_ssm",
       publisher:   "publisher_tesim",
       relation:    "collection_label_ssim",
-      rights:      "license_tesim",
+      rights:      "copyright_status_label_tesim",
       subject:     "lc_subject_label_tesim",
       title:       "title_tesim",
     },
+    dcterms: {
+      accessRights: "isGovernedBy_ssim",
+      alternative: "alternative_tesim",
+      isPartOf: "collection_label_ssim",
+      rightsHolder: "rights_holder_label_tesim",
+      spatial: "location_label_tesim",
+      type: "work_type_label_tesim",
+    },
     edm: {
-      isShownAt: lambda do |record|
+      isShownAt: "uri_ssm",
+      hasType: "form_of_work_label_tesim",
+      object: lambda do |record|
         (record["image_url_ssm"] || []).map do |path|
           "https://#{Rails.application.config.host_name}#{path}"
         end
       end,
+      rights: "license_tesim",
     },
   }.freeze
 
   def header_specification
     {
       "xmlns:dc" => "http://purl.org/dc/elements/1.1/",
+      "xmlns:dcterms" => "http://purl.org/dc/terms/",
       "xmlns:edm" => "http://www.europeana.eu/schemas/edm/",
-      "xmlns:oai_dc" => "http://www.openarchives.org/OAI/2.0/oai_dc/",
       "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
       "xsi:schemaLocation" => %(
-        https://digitalcollections.library.ucsc.edu/oai_dpla/
-        https://digitalcollections.library.ucsc.edu/oai_dpla/oai_dpla.xsd
+        https://alexandria.ucsb.edu/oai_cdl/
+        https://alexandria.ucsb.edu/oai_cdl.xsd
       ).gsub(/\s+/, " "),
     }
   end
